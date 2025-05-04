3cce737ed75cbd398f69d41784fad3d5c6ec8d2f
ashely-ra <ashely.green@gu.se>
2025-05-04 19:26:14 +0200

    output name fixes

--- ExportOBBTiles.py
+++ ExportOBBTiles.py
@@ -37,12 +37,11 @@ def create_dirs(output_dir,padded_dir,split_data):
             if not os.path.exists(os.path.join(padded_dir,'blank_imgs')):
                     os.makedirs(os.path.join(padded_dir,'blank_imgs'))
 
-# Tile large rasters to user-input dimensions
 def tile_raster(img_file,out_img_dir,tile_size,overlap,base_name):
     SplitRaster(img_file,out_img_dir,base_name,"SIZE_OF_TILE",
         "PNG","BILINEAR","#",f"{tile_size} {tile_size}",overlap
         )
-# Clip the label polygons for the full image to the extent of each tile, then make them rectangular again to comply with YOLO's OBB standard
+
 def clip_shapes(shp_file,out_img_dir,tile_img,ext_dir,out_bound):
     out_extent = tile_img.replace(out_img_dir,ext_dir).replace('.png','_EXTENT.shp')
     tile_extent = arcpy.ddd.RasterDomain(tile_img, out_extent, "POLYGON")
@@ -62,7 +61,7 @@ def clip_shapes(shp_file,out_img_dir,tile_img,ext_dir,out_bound):
         mbg_fields_option="NO_MBG_FIELDS"
     )
 
-# Pad the tiled image to the extent of the recalculated polygons using data from the original image
+
 def pad_tile(full_img,tile_file,base_name,shp_bound_dir,out_file,pad_dir):
     try:
         geom_file=f"{shp_bound_dir}/_{base_name}.shp"
@@ -97,13 +96,15 @@ def pad_tile(full_img,tile_file,base_name,shp_bound_dir,out_file,pad_dir):
             Raster_Format="PNG"
         )
 
-#  Convert the polygons to YOLO OBB text label files
+        
 def create_labels(img_file,min_area,padded_img_dir,blank_img_dir,geom_file,out_lbl_dir,base_name):
     raster_layer = arcpy.Describe(img_file)
+    # geom_layer = arcpy.Describe(geom_file)
     label_file = f"{out_lbl_dir}/{base_name}.txt"
 
     raster_layer = arcpy.Describe(img_file)
     rast_ext = raster_layer.extent
+    # geom_ext = geom_layer.extent
     
     xmin = rast_ext.XMin
     xmax = rast_ext.XMax
@@ -119,7 +120,9 @@ def create_labels(img_file,min_area,padded_img_dir,blank_img_dir,geom_file,out_l
     rast_ext.YMax = height
 
     new_xmin = rast_ext.XMin
+    # new_xmax = rast_ext.XMax
     new_ymin = rast_ext.YMin
+    # new_ymax = rast_ext.YMax
 
     width = (xmax-xmin)
     height = (ymax-ymin)
@@ -147,20 +150,26 @@ def create_labels(img_file,min_area,padded_img_dir,blank_img_dir,geom_file,out_l
             shutil.move(img_file,img_file.replace(padded_img_dir,blank_img_dir))
 
     else:
-        shutil.move(img_file,img_file.replace(padded_img_dir,blank_img_dir)) 
+        shutil.move(img_file,img_file.replace(padded_img_dir,blank_img_dir))
+
+# def script_tool(img_dir,out_img_dir,shape_dir,min_area,padded_img_dir,blank_img_dir,padded_label_dir,tile_size,overlap):
+    # Tile all images and add the base names to a list for the remaining processing steps
+    
 
 
 if __name__ == "__main__":
+
+    
     working_dir = arcpy.GetParameterAsText(0)
     main_img_dir = arcpy.GetParameterAsText(1)
+    # label_dir = arcpy.GetParameterAsText(2)
     shape_dir = arcpy.GetParameterAsText(2)
+    # gdb = arcpy.GetParameterAsText(4)
     tile_size = arcpy.GetParameterAsText(3)
     overlap = arcpy.GetParameterAsText(4)
     split = arcpy.GetParameterAsText(5)
+    # img_suffix = arcpy.GetParametersAsText(6)
     min_area = arcpy.GetParameterAsText(6)
-
-    arcpy.AddMessage(split)
- 
 
     output_dir = f"{working_dir}/yolo_obb_tiled_{tile_size}sz_{overlap}ov/"
     out_img_dir = f"{output_dir}/images"
@@ -177,36 +186,34 @@ if __name__ == "__main__":
     blank_img_dir = f"{padded_dir}/blank_imgs"
     create_dirs(output_dir,padded_dir,split)
     arcpy.AddMessage('Running script tool...')
+    # script_tool(img_dir,out_img_dir,shape_dir,min_area,padded_img_dir,blank_img_dir,padded_label_dir,tile_size,overlap)
     img_dir = f"{main_img_dir}/*/*" if split else f"{main_img_dir}/*"
-    for file in glob(img_dir):
-        if str(os.path.splitext(file)[1]).lower() in ['.png','.jpg','.jpeg','.tif','.tiff']:
-            # arcpy.AddMessage(file)
-            if split == 'true':
-                split_val = file.split('//')[-1].split('\\')[-2]
-            else:
-                split_val=''
-            base_img_dir = f"{out_img_dir}/{split_val}"
+    # arcpy.AddMessage(glob(img_dir))
+    file_list = [file for file in glob(img_dir) if str(os.path.splitext(file)[1]).lower() in ['.png','.jpg','.jpeg','.tif','.tiff']]
+    arcpy.SetProgressor("step", "Tiling data...",
+                    0, len(file_list), 1)
 
-            arcpy.AddMessage(base_img_dir)
+    for file in file_list:
+        if split == 'true':
+            split_val = file.split('//')[-1].split('\\')[-2] #str(os.path.split(file)[0]).split('/\/')[-1]
+        else:
+            split_val=''
+        base_img_dir = f"{out_img_dir}/{split_val}"
 
-            base_name = os.path.splitext(os.path.split(file)[-1])[0]
-            tile_raster(file,base_img_dir,tile_size,overlap,base_name)
-            arcpy.AddMessage(base_name)
-            for tile in glob(f"{base_img_dir}/{base_name}*.png"):
-                    
-                tile_name = os.path.splitext(os.path.split(tile)[0])[0]
-                shp_file = f"{shape_dir}/{split_val}/{base_name}.shp"
-                shp_bound_dir = f"{out_bound_dir}/{split_val}"
-                out_file=f"{padded_tif_dir}/{split_val}/{base_name}"
-                ext_dir=f"{out_ext_dir}/{split_val}"
-                pad_dir = f"{padded_img_dir}/{split_val}"
-                lbl_dir= f"{padded_label_dir}/{split_val}"
+        base_name = os.path.splitext(os.path.split(file)[-1])[0]
+        tile_raster(file,base_img_dir,tile_size,overlap,base_name)
+        arcpy.AddMessage(base_name)
+        for tile in glob(f"{base_img_dir}/{base_name}*.png"):
                 
-                
-
-                arcpy.AddMessage(tile_name)
-                arcpy.AddMessage(shp_file)
-                
-                clip_shapes(shp_file,base_img_dir,tile,ext_dir,out_bound=f"{shp_bound_dir}/{base_name}.shp")
-                pad_tile(file,tile,base_name,shp_bound_dir,out_file,pad_dir)
-                create_labels(tile,min_area,padded_img_dir,blank_img_dir,shp_file,lbl_dir,base_name)
+            tile_name = os.path.splitext(os.path.split(tile)[1])[0]
+            shp_file = f"{shape_dir}/{split_val}/{base_name}.shp"
+            shp_bound_dir = f"{out_bound_dir}/{split_val}"
+            out_file=f"{padded_tif_dir}/{split_val}/{tile_name}"
+            ext_dir=f"{out_ext_dir}/{split_val}"
+            pad_dir = f"{padded_img_dir}/{split_val}"
+            lbl_dir= f"{padded_label_dir}/{split_val}"
+            
+            clip_shapes(shp_file,base_img_dir,tile,ext_dir,out_bound=f"{shp_bound_dir}/{base_name}.shp")
+            pad_tile(file,tile,base_name,shp_bound_dir,out_file,pad_dir)
+            create_labels(tile,min_area,padded_img_dir,blank_img_dir,shp_file,lbl_dir,tile_name)
+            arcpy.SetProgressorPosition()
